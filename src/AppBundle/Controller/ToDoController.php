20169106fb7def8e8fcbcf720b08019cb9ecd862
Trajche <roskoskitrajce@gmail.com>
2017-11-03 15:45:09 +0100

    added task form

--- src/AppBundle/Controller/ToDoController.php
+++ src/AppBundle/Controller/ToDoController.php
@@ -2,9 +2,13 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\ToDoItem;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 
 class ToDoController extends Controller
@@ -14,9 +18,22 @@ class ToDoController extends Controller
     * @Method("GET")
     */
     public function toDoAction(){
+        
+        // create a task and give it some dummy data for this example
+        $task = new ToDoItem();
+        $task->setDescription('Write a blog post');
+        $task->setDate(new \DateTime('tomorrow'));
+        
+        $form = $this->createFormBuilder($task)
+        ->add('description', TextareaType::class)
+        ->add('date', DateType::class, array('widget' => 'single_text'))
+        ->add('save', SubmitType::class, array('label' => 'Create Task'))
+        ->getForm();
+        
         return $this->render('base.html.twig', array(
             'title'=>'HomePage',
-            'message'=>'Welcome to HomePage'
+            'message'=>'Welcome to HomePage',
+            'form'=>$form->createView()
         ));
     }
     /**
