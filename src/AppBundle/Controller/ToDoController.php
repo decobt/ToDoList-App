081493593ebbad2c576782a4d089ab1f9c13afaf
Trajche <roskoskitrajce@gmail.com>
2017-11-03 15:19:55 +0100

    added base.html.twig

--- src/AppBundle/Controller/ToDoController.php
+++ src/AppBundle/Controller/ToDoController.php
@@ -4,15 +4,20 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 
 class ToDoController extends Controller
 {
     /**
     * @Route("/", name="homepage")
+    * @Method("GET")
     */
     public function toDoAction(){
-        return new Response("hello");
+        return $this->render('base.html.twig', array(
+            'title'=>'HomePage',
+            'message'=>'Welcome to HomePage'
+        ));
     }
     /**
     * @Route("/edit/{post}")
